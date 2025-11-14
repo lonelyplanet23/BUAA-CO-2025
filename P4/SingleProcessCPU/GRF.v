94cd776e8d52f21b1b515be1572a81381b1911b4
lonelyplanet23 <276286687@qq.com>
2025-11-14 12:26:27 +0800

    GRF 实现，记得加入调试代码

--- P4/SingleProcessCPU/GRF.v
+++ P4/SingleProcessCPU/GRF.v
@@ -23,12 +23,31 @@ module GRF(
     input [4:0] A2,
     output [31:0] RD1,
     output [31:0] RD2,
+    input [31:0] PC,
     input [4:0] A3,
     input [31:0] WD,
     input WE,
     input clk,
-    input Reset
+    input reset
     );
 
+    reg [31:0] reg_file[31:0];
+    integer i;
+    always @(posedge clk) begin
+        if(reset) begin
+            for(i = 0; i < 32; i = i + 1)begin
+                reg_file[i] <= 32'h00000000;
+            end
+        end
+        else if(WE) begin
+            if(A3 != 5'b00000) begin
+                reg_file[A3] <= WD; //保护零号寄存器 一直为0
+                $display("@%h: $%d <= %h", PC, A3, WD);
+            end
+        end
+    end
+
+    assign RD1 = reg_file[A1];
+    assign RD2 = reg_file[A2];
 
 endmodule
